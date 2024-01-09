77ef4e89e7ba4f2d86dfd15f63363a8f15591856
carozum <czumbiehlfaure@hotmail.fr>
2024-01-09 09:44:52 +0100

    ajout des news et de la quotation avec gestion du cas où la société saisie ne correspond à aucun ticker

--- app.py
+++ app.py
@@ -1,34 +1,52 @@
 from flask import Flask, render_template, request
 from models import db, User, app
+from dotenv import dotenv_values
+import requests
+import json
+import numpy as np
+import pandas as pd
 
 
-def add_user_if_not_exists(prenom, nom, sexe, pseudo, titre):
-    existing_user = User.query.filter_by(pseudo=pseudo).first()
+config = dotenv_values(".env")
+AUTH_KEY = config['API_KEY']
+url_news = 'https://devapi.ai/api/v1/markets/news'
+url_quotes = 'https://devapi.ai/api/v1/markets/quote'
 
-    if existing_user is None:
-        new_user = User(prenom=prenom, nom=nom, sexe=sexe,
-                        pseudo=pseudo, titre=titre)
-        db.session.add(new_user)
-        db.session.commit()
-        return "User added successfully."
-    else:
-        return "User already exists."
+
+def trouver_ticker(company):
+    df = pd.read_csv('ticker.csv', sep=';')
+    df['Name'] = df['Name'].str.lower()
+    search = df[df['Name'].str.contains(company.lower())].head(1).Symbol
+
+    return search.values[0]
+
+
+def trouver_api(ticker, url):
+    params = {'ticker': ticker}
+    headers = {'Authorization': f'Bearer {AUTH_KEY}'}
+    response = requests.request('GET', url, headers=headers, params=params)
+    return response.json()
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    # Etape 1 - Recueil des données du formulaire
     if request.method == 'POST':
         prenom = request.form['prenom']
         nom = request.form['nom']
         sexe = request.form['sexe']
         pseudo = request.form['pseudo']
         titre = "Mr" if sexe == "homme" else "Mme"
+        company = request.form['companyName']
+        message = f"Bonjour {titre} {prenom} {nom}, votre nom d'utilisateur est {pseudo}."
 
+        # Etape 2 - Vérification que le pseudo n'est pas déjà dans la base
         users = User.query.all()
         pseudo_list = []
         for user in users:
             pseudo_list.append(user.pseudo)
 
+        # Etape 2 - le cas échéant ajout de l'utilisateur dans la base
         if pseudo not in pseudo_list:
             new_user = User(prenom=prenom,
                             nom=nom,
@@ -38,12 +56,24 @@ def index():
             db.session.add(new_user)
             db.session.commit()
 
-        message = f"Bonjour {titre} {prenom} {nom}, votre nom d'utilisateur est {pseudo}."
-
         if pseudo in pseudo_list:
             message += " Vous êtes déjà inscrit sur le site."
 
-        return render_template('bienvenue.html', message=message)
+        # Etape 3 - convertir la company recherchée en ticker
+        if company is not None:
+            try:
+                ticker = trouver_ticker(company)
+                # sortir de l'API les news concernant le ticker (symbol) demandé
+                news = trouver_api(ticker, url_news)
+                # sortir de l'API les chiffres de valorisation du symbol demandé
+                quotes = trouver_api(ticker, url_quotes)
+                return render_template('bienvenue.html', message=message, news=news, quotes=quotes, company=company)
+            except IndexError:
+                news = {}
+                quotes = {}
+                return render_template('bienvenue.html', message=message, news=news, quotes=quotes)
+
+            # enregistrer le log dans la base de donnée
 
     return render_template('index.html')
 
@@ -63,3 +93,14 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         app.run(debug=True, port=8000, host='127.0.0.1')
+
+
+# existing_user = User.query.filter_by(pseudo=pseudo).first()
+        # if existing_user is None:
+        #     new_user = User(prenom=prenom, nom=nom, sexe=sexe,
+        #                 pseudo=pseudo, titre=titre)
+        #     db.session.add(new_user)
+        #     db.session.commit()
+        #     return render_template('bienvenue.html', message=message)
+        # else:
+        #     return render_template('index.html')
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -13,3 +14,15 @@ class User(db.Model):
     sexe = db.Column('Sexe', db.String())
     pseudo = db.Column('Pseudo', db.String())
     titre = db.Column('Titre', db.String())
+
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user = db.Column('User', db.String())
+    date = db.Column('Date', db.DateTime,
+                     default=datetime.datetime.now)
+    saisie = db.Column('Saisie', db.String())
+    symbol = db.Column('Symbol', db.String())
+    company = db.Column('Company', db.String())
+    exchange = db.Column('Exchange', db.String())
+    lastSalePrice = db.Column('Price', db.String())
